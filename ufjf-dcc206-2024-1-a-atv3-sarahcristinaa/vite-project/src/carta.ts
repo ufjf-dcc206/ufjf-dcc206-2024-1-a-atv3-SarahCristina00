50882ab69e86f2225836a73b7ebda4965fa9f90f
Sarah Cristina Freitas da Silva <163673649+SarahCristina00@users.noreply.github.com>
2024-10-03 10:38:30 -0300

    Lança carta.ts, mains.ts e json

---
+++ ufjf-dcc206-2024-1-a-atv3-sarahcristinaa/vite-project/src/carta.ts
@@ -0,0 +1,31 @@
+export type Pokemon = { 
+    id: number;
+    nome: string;
+    tipo: string;
+    imagem: string;
+};
+
+export default class Carta extends HTMLElement {
+    constructor() {
+        super();
+        console.log("Criando card pokemon");
+    }
+
+    connectedCallback() {
+        const nome = this.getAttribute("nome");
+        const tipo = this.getAttribute("tipo");
+        const id = this.getAttribute("id");
+        const imagem = this.getAttribute("imagem");
+
+        this.innerHTML = `
+            <div>
+                <img src="${imagem}" alt="${nome}" />
+                <div>ID: ${id}</div>
+                <div>Nome: ${nome}</div>
+                <div>Tipo: ${tipo}</div>
+            </div>
+        `;
+    }
+}
+
+    
